af866b122a796dfa777ad8f7c69668905dd5b06d
abogovski <abogovski@gmail.com>
2017-08-24 00:16:11 +0300

    Add request functions for TelegramBot API
    Implement Get, Post, PostJSON, PostURLEncoded, PostMultipartForm

---
+++ tgbot/requests.go
@@ -0,0 +1,111 @@
+package tgbot
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+// Available HTTP request types:
+// 	URL query string
+// 	application/x-www-form-urlencoded
+// 	application/json (except for uploading files)
+// 	multipart/form-data (use to upload files)
+
+type request struct {
+	method       string
+	url          string
+	status       string
+	response     string
+	finishedWith string
+}
+
+func newRequest(method string, url string) *request {
+	st := &request{}
+	st.method = method
+	st.url = url
+	return st
+}
+
+func (r request) log() {
+	log.Printf("Request finished\n\t%v %v\n\tStatus: %v\n\tResponse: %v\n\tFinishedWith: %v\n",
+		r.method, r.url, r.status, r.response, r.finishedWith)
+}
+
+func (r *request) process(httpResponse *http.Response, err error) (*Response, error) {
+	defer func(r *request) { r.log() }(r)
+
+	if err != nil {
+		r.finishedWith = err.Error()
+		return nil, errors.New("tgbot " + r.method + " request failed: " + err.Error())
+	}
+	r.status = httpResponse.Status
+
+	// deserialize http response
+	response := &Response{}
+	decoder := json.NewDecoder(httpResponse.Body)
+	err = decoder.Decode(response)
+	if err != nil {
+		buffered, _ := ioutil.ReadAll(decoder.Buffered())
+		pending, _ := ioutil.ReadAll(httpResponse.Body)
+		r.response = string(buffered) + string(pending)
+		r.finishedWith = fmt.Sprintf("Failed to unmarshal TelegramBotAPI response: %v\n", err)
+		return nil, errors.New("tgbot http " + r.method + " request: " + err.Error())
+	}
+
+	remarshaledResponse, _ := json.Marshal(response)
+	r.response = string(remarshaledResponse)
+	r.finishedWith = "Success"
+	return response, nil
+}
+
+// Get GET
+func Get(botAPIURL string, methodName string, params Params) (*Response, error) {
+	url := botAPIURL + methodName
+	if len(params) > 0 {
+		urlValues, err := params.URLValues()
+		if err != nil {
+			return nil, errors.New("tgbot.Get: " + err.Error())
+		}
+		url = url + "?" + urlValues.Encode()
+	}
+
+	return newRequest("GET", url).process(http.Get(url))
+}
+
+// Post POST
+func Post(botAPIURL string, methodName string, contentType string, contentReader io.Reader) (*Response, error) {
+	url := botAPIURL + methodName
+	return newRequest("POST", url).process(http.Post(url, contentType, contentReader))
+}
+
+// PostURLEncoded POST application/x-www-form-urlencoded
+func PostURLEncoded(botAPIURL string, methodName string, params Params) (*Response, error) {
+	contentReader, err := params.URLEncode()
+	if err != nil {
+		return nil, errors.New("tgbot.PostURLEncoded: " + err.Error())
+	}
+	return Post(botAPIURL, methodName, "application/x-www-form-urlencoded", contentReader)
+}
+
+// PostJSON POST application/json
+func PostJSON(botAPIURL string, methodName string, params Params) (*Response, error) {
+	contentReader, err := params.JSONEncode()
+	if err != nil {
+		return nil, errors.New("tgbot.PostJSON: " + err.Error())
+	}
+	return Post(botAPIURL, methodName, "application/json", contentReader)
+}
+
+// PostMultipartForm POST multipart/form
+func PostMultipartForm(botAPIURL string, methodName string, params Params) (*Response, error) {
+	contentReader, err := params.MultipartFormEncode()
+	if err != nil {
+		return nil, errors.New("tgbot.PostMultipartForm: " + err.Error())
+	}
+	return Post(botAPIURL, methodName, "multipart/form-data", contentReader)
+}
---
+++ tgbot/requests_test.go
@@ -0,0 +1,97 @@
+package tgbot
+
+import (
+	"testing"
+)
+
+func TestGETNoParamsOk(t *testing.T) {
+	APIURL, err := LoadBotAPIURL(tokenFname)
+	if err != nil {
+		t.Fatal("Can't load API URL: " + err.Error())
+		return
+	}
+
+	response, err := Get(APIURL, "getMe", Params{})
+	if err != nil {
+		t.Fatal("GET request to telegramBotAPI failed: " + err.Error())
+		return
+	}
+
+	if !response.Ok {
+		t.Fatal("GET getMe response.Ok == false")
+		return
+	}
+}
+
+func TestGetWithParamsOk(t *testing.T) {
+	APIURL, err := LoadBotAPIURL(tokenFname)
+	if err != nil {
+		t.Fatal("Can't load API URL: " + err.Error())
+		return
+	}
+
+	response, err := Get(APIURL, "getMe", Params{"limit": 5})
+	if err != nil {
+		t.Fatal("GET request to telegramBotAPI failed: " + err.Error())
+		return
+	}
+
+	if !response.Ok {
+		t.Fatal("GET getMe response.Ok == false")
+		return
+	}
+}
+
+func TestGetNotFound(t *testing.T) {
+	APIURL := GenBotAPIURL("Invalid123ID:Invalid567Token")
+	response, err := Get(APIURL, "getMe", Params{})
+	if err != nil {
+		t.Fatal("GET request to telegramBotAPI failed: " + err.Error())
+		return
+	}
+
+	if response.Ok {
+		t.Fatal("GET get response.Ok == true")
+		return
+	}
+}
+
+func TestPostUrlEncodedParamsOk(t *testing.T) {
+	APIURL, err := LoadBotAPIURL(tokenFname)
+	if err != nil {
+		t.Fatal("Can't load API URL: " + err.Error())
+		return
+	}
+
+	response, err := PostURLEncoded(APIURL, "getMe", Params{"limit": 5})
+	if err != nil {
+		t.Fatal("GET request to telegramBotAPI failed: " + err.Error())
+		return
+	}
+
+	if !response.Ok {
+		t.Fatal("GET getMe response.Ok == false")
+		return
+	}
+}
+
+func TestPostJSONParamsOk(t *testing.T) {
+	APIURL, err := LoadBotAPIURL(tokenFname)
+	if err != nil {
+		t.Fatal("Can't load API URL: " + err.Error())
+		return
+	}
+
+	response, err := PostJSON(APIURL, "getMe", Params{"limit": 5})
+	if err != nil {
+		t.Fatal("GET request to telegramBotAPI failed: " + err.Error())
+		return
+	}
+
+	if !response.Ok {
+		t.Fatal("GET getMe response.Ok == false")
+		return
+	}
+}
+
+// TODO: add TestMultipartFormParamsOk when Params.MultipartFormEncode implemented
