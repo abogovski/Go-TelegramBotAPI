22c660d2e76278cbef9956f2e6afcd8f88e0dac9
abogovski <abogovski@gmail.com>
2017-08-24 02:29:20 +0300

    Add safe response.result getters for User, Message, []Update types

--- tgbot/responses.go
+++ tgbot/responses.go
@@ -1,6 +1,9 @@
 package tgbot
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"errors"
+)
 
 // ResponseParameters https://core.telegram.org/bots/api/#responseparameters
 type ResponseParameters struct {
@@ -16,4 +19,69 @@ type Response struct {
 	Description *string             `json:"description,omitempty"`
 	Parameters  *ResponseParameters `json:"parameters,omitempty"`
 	Result      *json.RawMessage    `json:"result,omitempty"`
+}
+
+// GetRawResult safely gets Result from Ok==true response
+func (response Response) GetRawResult() (*json.RawMessage, error) {
+	if response.Ok == false {
+		return nil, errors.New("tgbot.Response.GetRawResult: Ok is false")
+	}
+
+	if response.Result == nil {
+		return nil, errors.New("tgbot.Response.GetRawResult: Result is nil, although Ok is true")
+	}
+
+	if len(*response.Result) == 0 {
+		return nil, errors.New("tgbot.Response.GetRawResult: zero len Result")
+	}
+
+	return response.Result, nil
+}
+
+// GetResultUser safely gets Result from Ok==true response as User
+func (response Response) GetResultUser() (*User, error) {
+	result, err := response.GetRawResult()
+	if err != nil {
+		return nil, errors.New("tgbot.Response.GetResultUser: " + err.Error())
+	}
+
+	var user User
+	err = json.Unmarshal(*result, &user)
+	if err != nil {
+		return nil, errors.New("tgbot.Response.GetResultUser unmarshal result as User:" + err.Error())
+	}
+
+	return &user, nil
+}
+
+// GetResultUpdates safely gets Result from Ok==true response as []Update
+func (response Response) GetResultUpdates() ([]Update, error) {
+	result, err := response.GetRawResult()
+	if err != nil {
+		return nil, errors.New("tgbot.Response.GetResultUpdates: " + err.Error())
+	}
+
+	var updates []Update
+	err = json.Unmarshal(*result, &updates)
+	if err != nil {
+		return nil, errors.New("tgbot.Response.GetResultUpdates unmarshal result as []Update:" + err.Error())
+	}
+
+	return updates, nil
+}
+
+// GetResultMessage safely gets Result from Ok==true response as Message
+func (response Response) GetResultMessage() (*Message, error) {
+	result, err := response.GetRawResult()
+	if err != nil {
+		return nil, errors.New("tgbot.Response.GetResultMessage: " + err.Error())
+	}
+
+	var message Message
+	err = json.Unmarshal(*result, &message)
+	if err != nil {
+		return nil, errors.New("tgbot.Response.GetResultMessage unmarshal result as Message:" + err.Error())
+	}
+
+	return &message, nil
 }
