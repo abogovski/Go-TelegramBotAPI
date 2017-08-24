fa1c39dce9966ac12cfa00ef784e1eeb945b2f26
abogovski <abogovski@gmail.com>
2017-08-25 00:04:54 +0300

    Fix pollUpdates stop

--- tgbot/pollupdates.go
+++ tgbot/pollupdates.go
@@ -29,17 +29,15 @@ func PollUpdatesCB(botAPIURL string, params Params, handleUpdates func([]Update,
 	params["limit"] = 1
 	params["timeout"] = 0
 	_, status, err := GetUpdates(botAPIURL, params)
-	if err != nil {
-		return offset, status, err
-	}
-	return offset, status, nil
+	return offset, status, err
 }
 
 // PollUpdates .
 func PollUpdates(botAPIURL string, params Params, output chan<- Update, stop *int32) (Integer, int, error) {
 	handleUpdates := func(updates []Update, offset Integer) (Integer, bool) {
+		pstop := stop
 		for _, update := range updates {
-			if atomic.LoadInt32(stop) > 0 {
+			if atomic.LoadInt32(pstop) > 0 {
 				log.Println("PollUpdates received stop.")
 				close(output)
 				return offset, false
